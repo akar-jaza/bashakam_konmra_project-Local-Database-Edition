76d9ed51994f904245db497abef453886b1ac8e8
Akar Jaza <akarslemany08@gmail.com>
2023-07-22 23:56:56 +0300

    fixes

--- lib/widgets/konmra_list_item.dart
+++ lib/widgets/konmra_list_item.dart
@@ -24,28 +24,54 @@ class SlemaniKonmraListItem extends StatelessWidget {
         return const SizedBox.shrink();
       }
       final departmentData = departments[itemIndex];
-      final university = departmentData['university'] as String?;
-      final collage = departmentData['collage_institute'] as String?;
-      final department = departmentData['department'] as String?;
+      final university = departmentData['university'];
+      final collage = departmentData['collage_institute'];
+      final department = departmentData['department'];
+
+      final pZankoline = 'زانکۆڵاین: ${departmentData['p_zankoline']}';
+      final pParallel = 'پاراڵێڵ: ${departmentData['p_parallel']}';
+      final pEwaran = departmentData['p_ewaran'] ?? '';
+      final hasNonWhitespaceCharacters = RegExp(r'\S').hasMatch(pEwaran);
+      final pEwaranText = hasNonWhitespaceCharacters
+          ? 'ئێواران: $pEwaran'
+          : 'ئێواران: بەردەست نیە';
+
+      final gZankoline = 'زانکۆڵاین: ${departmentData['g_zankoline']}';
+      final gParallel = 'پاراڵێڵ: ${departmentData['g_parallel']}';
+      final gEwaran = departmentData['g_ewaran'] ?? '';
+      final hasNonWhitespaceCharactersIngEwaran =
+          RegExp(r'\S').hasMatch(gEwaran);
+      final gEwaranText = hasNonWhitespaceCharactersIngEwaran
+          ? 'ئێواران: $pEwaran'
+          : 'ئێواران: بەردەست نیە';
+
       return ListTile(
         title: Text(
           '${university ?? ''} / ${collage ?? ''} / ${department ?? ''}',
-          style: const TextStyle(
-            color: ThemeColors.kWhiteTextColor,
-          ),
+          style:
+              const TextStyle(color: ThemeColors.kWhiteTextColor, fontSize: 18),
           textAlign: TextAlign.center,
         ),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const SizedBox(height: 20),
+            Text(
+              'پارێزگا/ $pZankoline - $pParallel - $pEwaranText',
+              style: const TextStyle(
+                color: ThemeColors.kGreyTextColor,
+                fontSize: 13,
+              ),
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 10),
             Text(
-              'پارێزگا: ${departmentData['p_zankoline']}',
-              style: const TextStyle(color: ThemeColors.kWhiteTextColor),
-            ),
-            Text(
-              'گشتی: ${departmentData['g_zankoline']}',
-              style: const TextStyle(color: ThemeColors.kWhiteTextColor),
+              'گشتی/ $gZankoline - $gParallel - $gEwaranText',
+              style: const TextStyle(
+                color: ThemeColors.kGreyTextColor,
+                fontSize: 13,
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
