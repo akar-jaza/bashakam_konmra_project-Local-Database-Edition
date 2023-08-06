680b2686ffb8890f094bd8ce5ba08beae6e91511
Akar Jaza <akarslemany08@gmail.com>
2023-08-06 16:08:58 +0300

    add FAB animation

--- lib/Screens/kamtrin_konmra/kamtrin_konmra_page.dart
+++ lib/Screens/kamtrin_konmra/kamtrin_konmra_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:connectivity/connectivity.dart';
+import '../../components/my_custom_modal_bottom_sheet.dart';
 import '../../components/my_floating_action_button.dart';
 import '../../constantes/them_colors.dart';
 import '../../components/my_appbar.dart';
@@ -40,7 +41,8 @@ class _KamtrinKonmraState extends State<KamtrinKonmra> {
   List<String> gEwaran = [];
 
   bool isLoading = false;
-  bool isFabVisible = false;
+  bool isFabVisible = true;
+
   @override
   void initState() {
     super.initState();
@@ -201,92 +203,18 @@ class _KamtrinKonmraState extends State<KamtrinKonmra> {
             ],
           ),
         ),
-        floatingActionButton: isFabVisible
-            ? MyFloatingActionButton(
-                onPressed: () {
-                  showCustomModalBottomSheet(context);
-                },
-              )
-            : null,
-      ),
-    );
-  }
-
-  Future<dynamic> showCustomModalBottomSheet(BuildContext context) {
-    return showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: ((context) {
-        return FractionallySizedBox(
-          heightFactor: 0.75,
-          child: Container(
-            decoration: const BoxDecoration(
-              color: ThemeColors.kBodyColor,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-            ),
-            child: Scaffold(
-              extendBody: true,
-              backgroundColor: Colors
-                  .transparent, // Set the Scaffold's background to transparent
-
-              appBar: PreferredSize(
-                preferredSize: const Size.fromHeight(25),
-                child: AppBar(
-                  automaticallyImplyLeading: false,
-                  surfaceTintColor: ThemeColors.kBodyColor,
-                  leading: null,
-                  title: Platform.isIOS
-                      ? Image.asset(
-                          "assets/images/drag_handle_ios.png",
-                          width: 40,
-                        )
-                      : Center(
-                          child: Image.asset(
-                            "assets/images/drag_handle_android.png",
-                            width: 45,
-                          ),
-                        ),
-                  backgroundColor: Colors
-                      .transparent, // Set the AppBar's background to transparent
-                  elevation: 0,
-                ),
-              ),
-              body: const SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(18.0),
-                      child: Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: Text(
-                          "- ئەم لیستی کۆنمرە هی سالی (٢٠٢١-٢٠٢٢)ـە. ساڵانە نوێ دەکرێتەوە.\n\n"
-                          "- وشەی (گشتی) واتە نمرەی پێویست بۆ قوتابیانی دەرەوی شار. ئەگەر تۆ لە سلێمانی دەژیت و دەتەوێت لە هەولێر یاخود دهۆک وەربگیرێیت ئەوا پێویستە سێری نمرەی (گشتی) بکەیت.\n\n"
-                          "- وشەی (پارێزگا) واتە نمرەی پێویست بۆ قوتابیانی ئەو پارێزگایە(ئەو شارە). ئەگەر تۆ قوتابیەکی شاری سلێمانیت، وە خوودی بەشەکە لە سلێمانیە، ئەوا تۆ پێویستە سێری نمرەی (پارێزگا) بکەیت.\n\n"
-                          "- ئەم ئەپڵیکەیشنە لە سەرەتای گەشەپێدان دایە، کاتێک بۆ بەشێک دەگەڕێیت تکایە چەند هەنگاوێک بگرە بەر:\n\n"
-                          "• هەندێجار بۆ بەشێک دەگەڕێیت، وەک (ڕووپێوان)، ئەگەر بنووسیت (روبیوان) هیچ ئەنجامێک پیشان نادرێت.\n\n"
-                          "• زیاتر لە ٥٠٠ بەش بوونی هەیە لەم ئەپڵیکەشینە، لە لایەن منی گەشەپێدەر نەنوسراون، من تەنها پیشانیان دەدەمەوە، ئەکرێ هەڵەی ڕێزمانی بوونی هەبێت لەناوی بەشەکاندا.\n\n"
-                          "• هەندێ بەشی تێدایە، وەک تەکنەلۆجیای زانیاری، بە ناوی تریش تۆمارکراوە وەک (IT). بۆیە ئاگاداری ئەوانەش بدە تکایە.\n\n"
-                          "• هەندێ جار تۆ بۆ بەشی (پەرستاری پێنجوێن) دەگەڕێیت، کە دەنووسیت (پەرستاری) بەتەنها، هیچ دەرەنجامێکی نابێت. بەڵکوو بنووسە (پەرستاری پ) پیشان دەدرێت.\n\n"
-                          "• ئەگەر بێزاربوویت لە سێرچ کردن(نوسینی بەشەکان) بەڵام هیچ دەرەنجامێکی نەهێنا، ئەکرێ هەڵەی ڕێزمانی بوونی هەبێت، یاخود ئەو بەشە بە ڕێزمانێکی هەڵە نوسرابێت. پێشنیار دەکەم خۆت بگەڕێیت بەناو لیستەکەدا تاوەکوو بەشی دڵخوازت بدۆزیتەوە.\n\n"
-                          "لەکاتی بوونی کێشە لە نمرە، ئپڵیکەیشن یاخود هەرشتێکی تر، پەیوەندی بە گەشەپێدەرەوە بکە.\n\n",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "rabar_009",
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+        floatingActionButton: AnimatedAlign(
+          duration: const Duration(milliseconds: 500),
+          alignment:
+              isFabVisible ? Alignment.bottomRight : const Alignment(1, 2),
+          curve: Curves.fastOutSlowIn,
+          child: MyFloatingActionButton(
+            onPressed: () {
+              showCustomModalBottomSheet(context);
+            },
           ),
-        );
-      }),
+        ),
+      ),
     );
   }
 }
